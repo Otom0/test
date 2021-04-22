ae8c7e07ad521602461f7a9bcf775747f5aba242
Robert Dolgopolov <siidousdart@gmail.com>
2021-04-22 13:49:56 +0500

    testing

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        callSum(5, 2);
+
+    }
+    static void  callSum(int a, int b){
+        int sum = a * b;
+        System.out.println(sum);
+    }
+
+}
